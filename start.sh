90e2fdf8c6862a5c7ee3b24765f4f650b3feff24
xgujjgf <122663496+xgujjgf@users.noreply.github.com>
2023-01-14 08:19:30 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=a4541728-2d78-47c7-a2a9-04dbce66f17e
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=words-solver.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
